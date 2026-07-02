2754e8973fe1c58ab0c7a2e18a8713fbe30ba046
abhinav soni <abhinavsoni316@gmail.com>
2026-07-02 14:23:55 +0530

    configured add method of model in  product controller

--- src/features/product/controllers/product.controller.js
+++ src/features/product/controllers/product.controller.js
@@ -9,10 +9,20 @@ export default class ProductController
     }
     
     addProduct(req,res) {
-       console.log(req.body);
-       console.log('this is post request');
-       res.status(200).send('request send successfully');
-        
+    console.log(req.body);
+    //    console.log('this is post request');
+    //    res.status(200).send('request send successfully');
+        const {name,price,sizes} = req.body;
+           
+        const newProduct = {
+            name,
+            price: parseFloat(price),
+            sizes : sizes.split(','),
+            imageURL: `/uploads/${req.file.filename}`,
+        }
+
+        const createdRecord = ProductModel.add(newProduct);
+        res.status(201).send(createdRecord)
     }
 
     rateProduct(req,res) {}
