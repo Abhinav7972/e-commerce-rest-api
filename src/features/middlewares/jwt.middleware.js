ea41fb6c7c8d4493efb5d1deaa25d90605c5d5a2
abhinav soni <abhinavsoni316@gmail.com>
2026-07-06 11:17:01 +0530

    added json web token middleware

---
+++ src/features/middlewares/jwt.middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+
+export const jwtAuth = (req,res,next) =>{
+    //read the token
+     const token = req.headers["authorization"];
+
+    //if no token error
+     if(!token)
+     {
+        res.status(401).send('unauthorised access')
+     }
+
+    //check if token is valid 
+    try 
+    {
+    const payload = jwt.verify(token,"SOtTZG4qtMbgsw1e1aTBNAoNEA/JNUhAAPJHIXQPCCs=");
+    console.log(payload)
+    }
+    catch(err)
+    {
+    return res.status(401).send('unauthorsed access');
+    }
+
+    //if valid call next middleware 
+     next()
+    //else give error
+}
